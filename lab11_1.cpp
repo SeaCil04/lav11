1ec336aee6fcb1f381ba83322bbd17ee6f939402
Seacil04 <everest1756@gmail.com>
2024-12-20 17:06:23 +0700

    I will A

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,35 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    string text,grade;
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.\n";
+    int factor = rand()%9;
+    for(int i = 0; i<3; i++){
+        getline(cin,text);
+    }
+    if(factor == 1){
+        grade = "A";
+    }
+    else if(factor == 2){
+        grade = "B+";
+    }else if(factor == 3){
+        grade = "B";
+    }else if(factor == 4){
+        grade = "C+";
+    }else if(factor == 5){
+        grade = "C";
+    }else if(factor == 6){
+        grade = "D+";
+    }else if(factor == 7){
+        grade = "D";
+    }else if(factor == 8){
+        grade = "F";
+    }else if(factor == 0){
+        grade = "W";
+    }
+    cout << "You will get "<<grade<<" in this 261102.";
+}
